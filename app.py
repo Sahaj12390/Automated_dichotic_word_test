64b2e87f037a0916b9f963c4322883a8dd92c438
Sahaj12390 <shettysahaj5523@gmail.com>
2024-09-11 17:48:40 +0530

    audio

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 AUDIO_DIR = "audio"
 
 
-audio_files = [os.path.join(AUDIO_DIR, f) for f in os.listdir(AUDIO_DIR) if f.endswith('.Wav')]
+audio_files = [os.path.join(AUDIO_DIR, f) for f in os.listdir(AUDIO_DIR) if f.endswith('.wav')]
 
 
 current_index = 0
